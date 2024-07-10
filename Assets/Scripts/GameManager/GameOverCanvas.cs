2ecc02adb8bf4d95e6f0d1633f1b796d5350c74a
Camilo José de Jesús Amador Cabarcas <138091417+xMetroi@users.noreply.github.com>
2024-07-10 09:55:57 -0500

    Game Loop Finished

--- Assets/Scripts/GameManager/GameOverCanvas.cs
+++ Assets/Scripts/GameManager/GameOverCanvas.cs
@@ -10,16 +10,29 @@ public class GameOverCanvas : MonoBehaviour
     [SerializeField] private TMP_Text timeSurvivedText;
     [SerializeField] private TMP_Text killsCounterText;
     [SerializeField] private GameObject gameOverCanvas;
-    
-    private void OnEnable()
+
+    [Header("Win Properties")]
+    [SerializeField] private TMP_Text winTimeSurvivedText;
+    [SerializeField] private TMP_Text winKillsCounterText;
+    [SerializeField] private GameObject winCanvas;
+
+    public void OnPlayerDeath()
     {
-        timeSurvivedText.text = $"Time survived: {60 - GameManager.instance.timeTimer}";
+        timeSurvivedText.text = $"Time survived: {(int) (60 - GameManager.instance.timeTimer)}";
         killsCounterText.text = $"Enemies killed: {GameManager.instance.killCounter}";
+    }
+
+    public void OnPlayerWin()
+    {
+        winKillsCounterText.text = $"Enemies killed: {GameManager.instance.killCounter}";
     }
 
     public void OnBackToLobbyButton()
     { 
         gameOverCanvas.SetActive(false);
+        winCanvas.SetActive(false);
+        GameManager.instance.killCounter = 0;
         SceneManager.LoadScene("MainMenu");
+        GameManager.instance.finalBossSpawned = false;
     }
 }
